c3625a0909555febdf02bf96b8fe2ea8d4b3e0fa
jordbort <jordan.bortner@gmail.com>
2022-12-05 20:49:29 -0500

    Plugged in API

--- src/pages/Brewery.js
+++ src/pages/Brewery.js
@@ -1,37 +1,37 @@
-import { useState, useEffect } from "react"
-import { useParams } from "react-router"
+// import { useState, useEffect } from "react"
+// import { useParams } from "react-router"
 
 const Brewery = (props) => {
-    const [brewery, setBrewery] = useState()
-    const selectedBrewery = useParams()
+    // const [brewery, setBrewery] = useState()
+    // const selectedBrewery = useParams()
     
-    handlerBreweryFetch = async () => {
-        const breweryFetch = await fetch (`https://api.openbrewerydb.org/breweries/${selectedBrewery.id}`)
-        const breweryData = breweryFetch.json()
-        setBrewery(breweryData)
-    }
+    // handlerBreweryFetch = async () => {
+    //     const breweryFetch = await fetch (`https://api.openbrewerydb.org/breweries/${selectedBrewery.id}`)
+    //     const breweryData = breweryFetch.json()
+    //     setBrewery(breweryData)
+    // }
     
-    useEffect (() => {
-    handlerBreweryFetch()
-    }, [])
+    // useEffect (() => {
+    // handlerBreweryFetch()
+    // }, [])
 
-    const { name, street, city, state, postal_code, country, longitude, latitude, phone, website_url, updated_at } = brewery
+    // const { name, street, city, state, postal_code, country, longitude, latitude, phone, website_url, updated_at } = brewery
 
-    if (!brewery) {
-        <p>Loading brewery info...</p>
-    }
+    // if (!brewery) {
+    //     <p>Loading brewery info...</p>
+    // }
 
-    return (
-        <div className="brewery-container">
-        <h2>{name}</h2>
-        <h3>{street}</h3>
-        <h3>{city}, {state} {postal_code}</h3>
-        <h3>{country}</h3>
-        {phone ? <h3>{phone}</h3> : null}
-        {website_url ? <h3><a href={website_url}>{website_url}</a></h3> : null}
-        <h4>last updated on: {updated_at}</h4>
-        </div>
-    )
+    // return (
+    //     <div className="brewery-container">
+    //     <h2>{name}</h2>
+    //     <h3>{street}</h3>
+    //     <h3>{city}, {state} {postal_code}</h3>
+    //     <h3>{country}</h3>
+    //     {phone ? <h3>{phone}</h3> : null}
+    //     {website_url ? <h3><a href={website_url}>{website_url}</a></h3> : null}
+    //     <h4>last updated on: {updated_at}</h4>
+    //     </div>
+    // )
 }
 
 export default Brewery
--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,8 +1,22 @@
 // import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 const SearchResults = (props) => {
+    const [results, setResults] = useState(null)
+    useEffect(() => {
+        const url = `https://api.openbrewerydb.org/breweries`
+        fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json)
+            setResults(json)
+        })
+    }, [])
+    if(!results) {
+        return <p>Loading search results...</p>
+    }
     return (
-        <h2>SearchResults here</h2>
+        <h2>Search results:</h2>
     )
 }
 
