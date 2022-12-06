041ec48f12b2912bbc0663371018865a7dee570d
coreyloftus <coreyloftus@gmail.com>
2022-12-06 12:31:08 -0500

    Create About page and Brewery.js work

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,11 +10,11 @@ const About = (props) => {
         </ul>
         <h3>for their Software Engineering Intensive, class of 10-31-22</h3>
         <h3>with General Assembly</h3>
-        <h3><a href="https://github.com/jordbort/project-2-brewery-app" target="_blank">Check out the Github Repo here</a></h3>
+        <h3><a href="https://github.com/jordbort/project-2-brewery-app" target="_blank" rel="noreferrer">Check out the Github Repo here</a></h3>
         <h4>Made possible thanks to the following APIs:</h4>
         <ul>
-            <li><a href="https://www.openbrewerydb.org" target="_blank">Open Brewery DB</a></li>
-            <li><a href="" target="_blank">Some Maps API</a></li>
+            <li><a href="https://www.openbrewerydb.org" target="_blank" rel="noreferrer">Open Brewery DB</a></li>
+            <li><a href="https://www.openbrewerydb.org" target="_blank" rel="noreferrer">Some Maps API</a></li>
         </ul>
         <h3>Please search responsibly.</h3>
         <h3>Cheers!</h3>
--- src/pages/Brewery.js
+++ src/pages/Brewery.js
@@ -5,12 +5,16 @@ import { useParams } from "react-router"
 const Brewery = (props) => {
     const [brewery, setBrewery] = useState([])
     const selectedBrewery = useParams()
+
     console.log(selectedBrewery.brewery)
     useEffect (() => {
         fetch (`https://api.openbrewerydb.org/breweries/${selectedBrewery.brewery}`)
         .then ((res) => res.json())
         .then ((json) => {
+        console.log(json)
         setBrewery(json)
+        console.log(brewery)
+        // console.log(brewery[0].name)
         })
     }, [])
 
