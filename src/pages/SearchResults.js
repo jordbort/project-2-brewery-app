137c4c225fbc80d16b6f1edf5a38a803d1faab71
jordbort <jordan.bortner@gmail.com>
2022-12-07 12:22:12 -0500

    Changing page number variable 'page' to 'pageNumber'

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom"
+import { Link, redirect } from "react-router-dom"
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 
 const SearchResults = (props) => {
-    let {page, perPage} = useParams()
+    let {pageNumber, perPage} = useParams()
     const [results, setResults] = useState(null)
     const [formState, setFormState] = useState(perPage)
     // const [radioState, serRadioState] = useState()
@@ -14,7 +14,7 @@ const SearchResults = (props) => {
 
     // API call and response
     useEffect(() => {
-        const url = `https://api.openbrewerydb.org/breweries?per_page=${perPage}&page=${page}`
+        const url = `https://api.openbrewerydb.org/breweries?per_page=${perPage}&page=${pageNumber}`
         fetch(url)
         .then((response) => response.json())
         .then((json) => {
@@ -23,7 +23,7 @@ const SearchResults = (props) => {
             console.log("Retrieved data:", json)
             setResults(json)
         })
-    }, [page, perPage]) //, [] <= this might need to be added back in, if you're getting into an infinite loop!!
+    }, [pageNumber, perPage]) //, [] <= this might need to be added back in, if you're getting into an infinite loop!!
 
     // Dropdown selection menu function
     const handleSelect = (event) => {
@@ -32,22 +32,25 @@ const SearchResults = (props) => {
         perPage = event.target.value
         setFormState({...formState, [event.target.name]: event.target.value})
         window.location.assign(`/breweries/per_page=${perPage}&page=1`)
+        // return redirect(`/breweries/per_page=${perPage}&page=1`)
     }
 
     const handlePrevPageClick = (event) => {
-        // console.log("prev page click WAS:", page)
-        if(page > 1) {
-            page--
+        // console.log("prev page click WAS:", pageNumber)
+        if(pageNumber > 1) {
+            pageNumber--
+            // return redirect(`/breweries/per_page=${perPage}&page=${pageNumber}`)
         }
-        // console.log("prev page click IS NOW:", page)
-        window.location.assign(`/breweries/per_page=${perPage}&page=${page}`)
+        // console.log("prev page click IS NOW:", pageNumber)
+        // window.location.assign(`/breweries/per_page=${perPage}&page=${pageNumber}`)
     }
 
     const handleNextPageClick = (event) => {
-        // console.log("next page click WAS:", page)
-        page++
-        // console.log("next page click IS NOW:", page)
-        window.location.assign(`/breweries/per_page=${perPage}&page=${page}`)
+        // console.log("next page click WAS:", pageNumber)
+        pageNumber++
+        // console.log("next page click IS NOW:", pageNumber)
+        // window.location.assign(`/breweries/per_page=${perPage}&page=${pageNumber}`)
+        // return redirect(`/breweries/per_page=${perPage}&page=${pageNumber}`)
     }
 
     if(!results) {
@@ -85,34 +88,36 @@ const SearchResults = (props) => {
                         </select>
                     </form>
                     <p></p>
-                    {Number(page)===1 ? <button>Prev Page</button> : <button onClick={handlePrevPageClick}>Prev Page</button>}
+                    {Number(pageNumber)===1 ? <button>Prev Page</button> : <button onClick={handlePrevPageClick}>Prev Page</button>}
                     <button onClick={handleNextPageClick}>Next Page</button>
-                    <p>Page number: {page}</p>
+                    <p>Page number: {pageNumber}</p>
                 </div>
 
                 {/* Search Results */}
-                <h2>Search results:</h2>
-                <ol>
-                    {results.map((brewery, idx) => {
-                        return (
-                            <Link to={'/brewery/' + brewery.id} key={idx}>
-                                <div className={brewery.brewery_type}>
-                                <li>{brewery.name ? brewery.name : null}</li>
-                                    <ul>
-                                        {brewery.street && brewery.street !== "Unnamed Street" ? <li>{brewery.street}</li> : null}
-                                        {brewery.address_2 ? <li>{brewery.address_2}</li> : null}
-                                        {brewery.address_3 ? <li>{brewery.address_3}</li> : null}
-                                        <li>{brewery.city ? brewery.city : null}{brewery.state ? `, ${brewery.state}` : null} {brewery.postal_code ? brewery.postal_code : null}</li>
-                                        {brewery.country && brewery.country !== "United States" ? <li>{brewery.country}</li> : null}
-                                        {brewery.phone ? <li>Phone: {brewery.phone}</li> : null}
-                                        {brewery.website ? <li>Website: {brewery.website}</li> : null}
-                                    </ul>
-                                    <p></p>
-                                </div>
-                            </Link>
-                        )
-                    })}
-                </ol>
+                <div className="all-search-results-box">
+                    <h2>Search results:</h2>
+                    <ol>
+                        {results.map((brewery, idx) => {
+                            return (
+                                <Link to={'/brewery/' + brewery.id} key={idx}>
+                                    <div className="search-result-box"> {/* may also use {brewery.brewery_type} here? */}
+                                    <li>{brewery.name ? brewery.name : null}</li>
+                                        <ul>
+                                            {brewery.street && brewery.street !== "Unnamed Street" ? <li>{brewery.street}</li> : null}
+                                            {brewery.address_2 ? <li>{brewery.address_2}</li> : null}
+                                            {brewery.address_3 ? <li>{brewery.address_3}</li> : null}
+                                            <li>{brewery.city ? brewery.city : null}{brewery.state ? `, ${brewery.state}` : null} {brewery.postal_code ? brewery.postal_code : null}</li>
+                                            {brewery.country && brewery.country !== "United States" ? <li>{brewery.country}</li> : null}
+                                            {brewery.phone ? <li>Phone: {brewery.phone}</li> : null}
+                                            {brewery.website ? <li>Website: {brewery.website}</li> : null}
+                                        </ul>
+                                        <p></p>
+                                    </div>
+                                </Link>
+                            )
+                        })}
+                    </ol>
+                </div>
                 <h4>^^^ END OF SEARCH RESULTS PAGE ^^^</h4>
             </>
         )
